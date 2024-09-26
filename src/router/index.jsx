43b77bfd921f595b32961e2dea056577ddad8927
ismail-is <ismailthalithanooji@gmail.com>
2024-09-26 14:48:32 +0530

    add

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -46,7 +46,7 @@ export const router = createBrowserRouter([
 				element: <LayoutOne />,
 				children: [
 					{
-						path: "/",
+						path: "/ms",
 						element: <HomeOne />,
 					},
 					{
